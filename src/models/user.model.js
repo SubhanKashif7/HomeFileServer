97d3f4a618ea3f57d01a3704836f478baadd895c
? <=>
2024-04-25 12:13:46 +0500

    maybe finished user.model.js

---
+++ src/models/user.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema({
+    username : {
+        type : String,
+        required : true,
+        lowercase : true
+    },
+
+    password : {
+        type : String,
+        required : true
+    }
+},{timeseries : true});
+
+userSchema.pre("save",async function(next){
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password,10);
+    next();
+});
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password,this.password);
+};
